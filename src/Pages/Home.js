520395dc8f2df247baff4563a74f52b7702e921c
vvictorious <vvictorious30@gmail.com>
2018-05-15 08:09:43 -0600

    added a button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,13 +8,9 @@ class Home extends Component {
 		return(
 			<div>
 				<Navbar />
-				 <img src={ photo } className= "photo" alt="Barber shop photo"/>;
-				<div className="container">
-					 
-					<h2></h2>
-					<p>
-					</p>
-				</div>
+				<img src={ photo } className= "photo" alt="Barber shop photo"/>;
+				<button class="button" href="https://www.schedulicity.com/scheduling/thsqf4">Book Online</button>
+
 			</div>
 
 		);
