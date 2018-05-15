1a657bc771a6a639349f258fcb3c5f5a4c3972a1
vvictorious <vvictorious30@gmail.com>
2018-05-15 08:29:27 -0600

    button works how I want it now

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
 			<div>
 				<Navbar />
 				<img src={ photo } className= "photo" alt="Barber shop photo"/>;
-				<button class="button" href="https://www.schedulicity.com/scheduling/thsqf4">Book Online</button>
+				<a class="button" target="_blank" href="https://www.schedulicity.com/scheduling/thsqf4">Book Online</a>
 
 			</div>
 
